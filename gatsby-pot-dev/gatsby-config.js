c22019de2262233b854dc5066e7fb537bfe3dbe5
Mika Hamalainen <mika@equstom.fi>
2019-01-18 10:54:03 +0200

    Added local images gatsby support

--- gatsby-pot-dev/gatsby-config.js
+++ gatsby-pot-dev/gatsby-config.js
@@ -37,6 +37,23 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     // {
     //   resolve: `gatsby-source-wordpress`,
     //   options: {
--- gatsby-pot-dev/src/pages/01-14-2019-foobar/index.md
+++ gatsby-pot-dev/src/pages/01-14-2019-foobar/index.md
@@ -6,3 +6,11 @@ image: ./rush.jpeg
 ---
 In life there will be road blocks but we will over come it. Special cloth alert. Don’t ever play yourself. The key to more success is to get a massage once a week, very important, major key, cloth talk.
 <pre><code>// some css grid code </code></pre> 
+Here's our logo (hover to see the title text):
+
+Inline-style: 
+![alt text](gatsby-astronaut.png "Logo Title Text 1")
+
+Reference-style: 
+![alt text][logo]
+[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
