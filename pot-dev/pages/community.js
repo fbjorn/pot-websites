9327bef1a5c0e19afd5c7753d976d91496f21cdf
kyyberi <jarkko@vimo.fi>
2019-01-25 09:39:40 +0200

    Cleanup, renamed gatsby folder, updated docker-setup, cmsadmin config

--- pot-dev/pages/community.js
+++
@@ -1,8 +0,0 @@
-const Community = () => (
-  <div>
-    <h1>Community</h1>
-    <p>Hello Twin with Next.js</p>
-  </div>
-)
-
-export default Community
--- pot-dev/pages/login.js
+++
@@ -1,8 +0,0 @@
-const Login = () => (
-  <div>
-    <h1>Login</h1>
-    <p>Hello Twin with Next.js</p>
-  </div>
-)
-
-export default Login
