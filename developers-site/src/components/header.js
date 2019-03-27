37c68e0b94229bea76ddb639891f03f820a9cb00
Mika Hamalainen <mika@equstom.fi>
2019-03-27 17:20:34 +0200

    Footer

--- developers-site/src/components/header.js
+++ developers-site/src/components/header.js
@@ -1,117 +1,84 @@
 import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import React from 'react'
-// import { Navbar, Nav, NavDropdown, Form, FormControl, Button  } from 'react-bootstrap';
 
 import Logo from "../images/platformoftrust-wide-white-rgb.svg"
+import Facebook from "../images/facebook.svg"
+import Twitter from "../images/twitter.svg"
+import Github from "../images/github.png"
+import Linkedin from "../images/linkedin.png"
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: true,
-      dropdown: false
-    };
-  }
+const Footer = () => (
+  <footer className="page-footer footer font-small pt-4" >
 
-  componentWillMount() {
-    this.setState({
-      collapsed: !this.state.collapsed,
-      dropdown: false
-    });
-  }
+    <svg width="0" height="0">
+      <clipPath id="clipPathSVG" clipPathUnits="objectBoundingBox">
+        <polygon points="0, 1, 0.05, 0, 0.95, 0, 1, 1"/>
+      </clipPath>
+    </svg>
 
-  componentDidUpdate(prevProps) {
-    console.log("Updated")
-  }
+    <div className="container-fluid text-center text-md-left">
+      <div className="row">
+      
+        <div className="col-md-3 offset-md-1">
+          <img src={Logo} className="logo__footer" alt="Logo" />
+        </div>
 
-  toggleNavbar = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-      dropdown: false
-    });
-  }
-  toggleDropdown = () => {  this.setState({ dropdown: !this.state.dropdown }); }
+        <div className="col-md-3 mt-md-0 mt-3">
 
-  render() {
-    const classMobileMenu = 'navbar-collapse' + (this.state.collapsed ? 'collapse' : '')
-    const classDropdownMenu = `dropdown-menu ${this.state.dropdown ? 'show' : ''}` 
-    const { collapsed, dropdown } = this.state
-    return (
-      <header className="Header">
-        <nav className="navbar navbar-expand-lg navbar-dark">
-          <Link className="navbar-brand" to="http://localhost:8001/">
-            <img src={Logo} className="logo__header" alt="Logo" />
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={this.toggleNavbar}>
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <h5 className="text-uppercase mb-3">Platform of Trust Oy</h5>
+          <p>VAT number: FI29800052</p>
+          <p><a href="https://www.tilaajavastuu.fi/en/data-protection/">Data protection</a></p>
+          <nav className="some-links mt-4">
+            <a href="#" class="some-link facebook"><img src={Facebook} className="some-icon" alt="Facebook" /></a>
+            <a href="#" class="some-link twitter"><img src={Twitter} className="some-icon" alt="Twitter" /></a>
+            <a href="#" class="some-link github"><img src={Github} className="some-icon" alt="Github" /></a>
+            <a href="#" class="some-link linkedin"><img src={Linkedin} className="some-icon" alt="Linkedin" /></a>
+          </nav>
 
-          <div className={classMobileMenu} id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.platformoftrust.net/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="http://builder-staging.oftrust.net/">My World</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="http://localhost:8001/">Market Place</a>
-              </li>
-              <li className="nav-item activedropdown">
-                <Link className="nav-link" to="/"  style={{ display: 'inline-block' }} >For developers</Link>
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={this.toggleDropdown}  style={{ display: 'inline-block' }} ></a>
-                <div className={classDropdownMenu} aria-labelledby="navbarDropdownMenuLink">
-                  <ul>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/use-cases">Use Cases</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/apis">APIs</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link" to="/blogs">Blogs</Link>
-                    </li> 
-                    </ul>  
-                </div>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/pricing">Pricing</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/news">News</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/events">Events</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link disabled" to="http://localhost:8001/">About us</Link>
-              </li>
-            </ul>
-            <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <ul className="nav navbar navbar-right">
-              <li>
-                <button className="btn btn-primary">
-                  <a className="nav-link" href="https://login.oftrust.net/">Login</a>
-                </button>
-              </li>
-            </ul>
-          </div>
-        </nav> 
-      </header>
-    )
-  }
-}
+        </div>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+        <nav className="sitemap col-md-2 mb-md-0 mb-3">
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+          <h5 className="text-uppercase">Pages</h5>
+          <ul className="list-unstyled">
+            <li>
+              <Link className="nav-link" to="/pricing">Pricing plans</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/news">News</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/events">Events</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/Team">Team</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default Header
+        <nav className="sitemap col-md-2 mb-md-0 mb-3">
+          <h5 className="text-uppercase">Services</h5>
+          <ul className="list-unstyled">
+            <li>
+              <a className="nav-link" href="http://preview.oftrust.net">Platform of Trust</a>
+            </li>
+            <li>
+              <a className="nav-link" href="http://market.oftrust.net/">Market place</a>
+            </li>
+            <li>
+              <a className="nav-link" href="http://world.oftrust.net/">My World</a>
+            </li>
+            <li>
+              <a className="nav-link" href="http://developer.oftrust.net/">Developer portal</a>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+
+    </div>
+  </footer>
+)
+
+export default Footer
