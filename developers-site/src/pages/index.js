c4fc7ab4b452e808a44721818a38d37773b9ddda
kyyberi <jarkko@vimo.fi>
2019-04-05 15:51:38 +0300

    cleanup

--- developers-site/src/pages/index.js
+++ developers-site/src/pages/index.js
@@ -15,253 +15,132 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Platform of Trust Developers" keywords={[`Platform of trust`, `developers`]} />
-      <section className="Home page-content">
-        <main className="main container">
+      <section className="Home fullwidth page-content">
+        <div className="jumbotron jumbotron-fluid"> 
+          <div className="container">
+            <h1 className="display-4">Build trusted products</h1>
+            <p className="lead">Platform of Trust gives access to both rich build environment data and customers.​</p>
+            <div className="set">
+              <form>
+                <input type="email" placeholder="Email" />
+                <button type="submit" className="btn btn-primary">Sign up</button>            
+              </form>
+            </div> 
+            <p><a href="https://docs.oftrust.net">View API documentation</a></p>
+            <p><Link to="/apis">View APIs</Link></p>
 
-          <section className="intro container"> 
-            <div className="">
-              <h1 className="display-4">Build trusted products</h1>
-              <p className="lead">Platform of Trust gives access to both rich build environment data and customers.​</p>
-              <div className="set">
-                <form>
-                  <input type="email" placeholder="Email" />
-                  <button type="submit" className="btn btn-primary">Sign up</button>            
-                </form>
-              </div> 
-              <p><a href="https://docs.oftrust.net">View API documentation</a></p>
-              <p><Link to="/apis">View APIs</Link></p>
-            </div>
-          </section>
+          </div>
+        </div>
+    
+        <main className="container">
 
-          <section  className="overview container">
-            <div className="">
-              <h2>From Data Products to Applications</h2>  
-              <p>An overview of the processes included in the End-to-end Developer eXperience</p>
-              <img src={EndToEnd} className="img__article--full" alt="End-to-end user experience illustration" />
-              <p>From below you'll find getting started guides for the processes. </p>
-            </div>    
+          <section  className="container">
+            <h2>From Data Products to Applications</h2>  
+            <p>An overview of the processes included in the End-to-end Developer eXperience</p>
+            <img src={EndToEnd} className="logo__header" alt="End-to-end developer experience (DX) illustration" />
+            <p>From below you'll find getting started guides for the processes. </p>    
           </section>
           
+          <h2>What do you want to do next?</h2>
+          <section className="container row equal">
           
-          <section className="guides container">
-            <div className="intro">
-              <h2>What do you want to do next?</h2>
-            </div>
-
-            <div className="cards row equal">
-
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header">
-                    <h3>CREATE DATA PRODUCTS</h3>
-                  </div>
+            <div className="col-sm d-flex">
+            <Link to="/guides/data-products">
+              <div className="card">
+                
+                  <div className="card-header"><h3>CREATE DATA PRODUCTS</h3></div>
                   <div className="card-body">
                     <p>Check out getting started package and add new data products with integrations for other developers to consume</p>
                   </div>
-                  <Link to="/guides/data-products">
-                    <div className="card-footer">
-                      <p>Learn more </p>
-                    </div>
-                  </Link>
-                </div> 
-              </div>
-              
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header"><h3>BUILD APPS</h3></div>
-                  <div className="card-body">
-                    <p>Read the getting started and build new applications on top of the data products.</p>
+                  <div className="card-footer">
+                    <p>Learn more >> </p>
                   </div>
-                  <Link to="/guides/build-apps">
-                    <div className="card-footer">
-                      <p>Learn more</p>
-                    </div>
-                  </Link>
-                </div>
               </div>
-
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header"><h3>USE STANDARD DATA MODELS</h3></div>
-                  <div className="card-body">
-                    <p>All data exposed by the platform must be harmonised and use standard data models. Get familiar and start using along with Translator component. </p>
-                    
-                  </div>
-                  <Link to="/guides/data-models">
-                    <div className="card-footer">
-                      <p>Learn more</p>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            
+              </Link>
             </div>
+            
+            <div className="col-sm d-flex">
+              <div className="card">
+                <div className="card-header"><h3>BUILD APPS</h3></div>
+                <div className="card-body">
+                  <p>Read the getting started and build new applications on top of the data products.</p>
+                </div>
+                <div className="card-footer">
+                  <Link to="/guides/build-apps">Learn more >> </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm d-flex">
+              <div className="card">
+                <div className="card-header"><h3>USE STANDARD DATA MODELS</h3></div>
+                <div className="card-body">
+                  <p>All data exposed by the platform must be harmonised and use standard data models. Get familiar and start using along with Translator component. </p>
+                  
+                </div>
+                <div className="card-footer">
+                <Link to="/guides/data-models">Learn more >> </Link>
+                </div>
+              </div>
+            </div>
+          </section> 
+
+ 
           
-            <div className="cards row equal">
+          <p>We are creating more guides on various topics. We also welcome community driven guides. Checkout how to contribute and see full list of guides.</p>
+          <div><button type="submit" className="btn btn-primary">Full list of guides! (/guides)</button></div>
+           
 
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header"><h3>SETUP TRANSLATOR</h3></div>
-                  <div className="card-body">
-                    <p>A Platform Of Trust translator standardizes the responses from a data source to the Platform itself.</p>
-                  </div>
-                  <Link to="/guides/translator">
-                    <div className="card-footer">
-                      <p>Learn more</p>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header"><h3>MANAGE DIGITAL TWINS</h3></div>
-                  <div className="card-body">
-                    <p>What is digital twin and why should I care? How to use digital twins in managing built environment data?</p>
-                  </div>
-                  <Link to="/guides/twins">
-                    <div className="card-footer">
-                      <p>Learn more</p>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-
-              <div className="col-md d-flex">
-                <div className="card">
-                  <div className="card-header"><h3>USE SANDBOX</h3></div>
-                  <div className="card-body">
-                    <p>Read the getting started and build new applications on top of the data products.</p>
-                  </div>
-                  <Link to="/guides/sandbox">
-                    <div className="card-footer">
-                      <p>Learn more</p>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-            </div>
-
-            <div className="outro">
-              <p>We are creating more guides on various topics. We also welcome community driven guides. Checkout how to contribute and see full list of guides.</p>
-              <Link className="nav-link__btn" to="/guides">Full list of guides! </Link>
-              {/* Use buttons as controls, not links
-              <button type="submit" className="btn btn-primary">
-                <Link className="nav-link__btn" to="/guides">Full list of guides! </Link>
-              </button> */}
-            </div>
-
-          </section>
-
-          <section className="apis container">
-            <div className="intro">
-              <h2>Get started with the Platform of Trust APIs in minutes</h2>
-              <p>Try out the most commonly used APIs instantly. Just select API, preferred code example and hit "run". The response with headers is printed below.</p>
-            </div>  
-            
-            <div className="row container p-0 pt-4">
-              <div className="col-md-6">
-                <div className="bd-clipboard">
-                  <button className="btn-clipboard" title="" data-original-title="Copy to clipboard">Copy</button>
-                </div>
-                <figure className="highlight">
-                  <pre>
-                    <code className="language-html" data-lang="html">
-                      <span className="nt">&lt;link</span>
-                      <br />
-                      <span className="na"> rel=</span>
-                      <span className="s">"stylesheet"</span> 
-                      <span className="na"> href=</span>
-                      <br />
-                      <span className="s">"https://foo.com.min.css"</span> 
-                      <span className="nt">&gt;</span>
-                    </code>
-                  </pre>
-                </figure>
-              </div>
-              <div className="col-md-6"> 
-                <button className="btn btn-primary btn-block">Context API</button>  
-                <button className="btn btn-primary btn-block">Identity API</button>   
-                <button className="btn btn-primary btn-block">Product API</button> 
-                <Link to="/apis">Read more about APIs and tools available </Link>                   
-              </div>
-            </div>
-          </section>
-
-          <section className="love container">
+          <section  className="container">
             <h2>We love developers</h2>  
             <p>Developer experience is at the forefront of everything we do. We strive to make products 
                 that are both delightful and get out of your way, so that you can focus on what you do best - 
                 building great apps and data products. </p>
                 
-            <p>Take a deep dive into API documentation including code examples for Python, Node.js, Java and shell (curl)</p>
+            <p>Take a deep dive into API documentation including code examples for shell (curl)</p>
+            
+            <p><a href="https://docs.oftrust.net">View API documentation >></a></p>
 
-            <Link to="/apis">Visit the API docs! </Link>  
-            {/* Use buttons as controls, not links
-            <button type="submit" className="btn btn-primary mt-3">Visit the API docs!</button> */}
+            <p>
+            Platform of Trust APIs and libraries. Get familiar with the APIs and start building trusted data products and applications on top our platform.   
+            </p>
+
+            <Link to="/apis">Read more about APIs and tools available >> </Link>  
+                
           </section>
-        
-          <section className="use-cases container">
-            <div className="title">
-              <h2>Learn why developers and data providers are choosing Platform of Trust</h2>
-            </div>
-              
-            <div className="cards row equal">
-              <div className="col-md d-flex">
+      
+          <section  className="container row">
+            <h2>Learn why developers and data providers are choosing Platform of Trust</h2>
+            <div className="container row equal">
+              <div className="col-sm d-flex">
                 <div className="card">
                   <div className="card-header"><h3>DATA PRODUCT USE CASE</h3></div>
                   <div className="card-body">
                     <p>Check out getting started package and add new data products with integrations for other developers to consume</p>
                   </div>
-                  <Link to="/use-cases/data-product">
-                    <div className="card-footer">
-                      <p>Learn more </p>
-                    </div>
-                  </Link>
-                </div>
+                  <div className="card-footer">
+                    <Link to="/use-cases/data-product">Learn more >> </Link>
+                  </div>
               </div>
-              <div className="col-md d-flex">
+              </div>
+              <div className="col-sm d-flex">
                 <div className="card">
                   <div className="card-header"><h3>APP USE CASE</h3></div>
                   <div className="card-body">
                     <p>Read the getting started and build new applications on top of the data products.</p>
                   </div>
-                  <Link to="/use-cases/build-application">
-                    <div className="card-footer">
-                      <p>Learn more </p>
-                    </div>
-                  </Link>
+                  <div className="card-footer">
+                    <Link to="/use-cases/build-application">Learn more >> </Link>
+                  </div>
                 </div>
               </div>
             </div>          
           </section> 
+          <section  className="container">
+            <h2>Stay informed and participate</h2>  
+            <p>Our daily communication is based on Slack and mailing list. Subscibe now and stay informed!</p>
+            <a href="https://www.platformoftrust.net/set-subscription">Subscribe >> </a>
+          </section>
           
-          <section  className="join container">
-            <div className="intro">
-              <h2>Stay informed and participate</h2>  
-              <p>Our daily communication is based on Slack and mailing list. Subscibe now and stay informed!</p>
-              <a href="https://www.platformoftrust.net/set-subscription">Subscribe </a>
-            </div>
-          </section>
-
-          <section  className="sign-up container">
-            <div className="row">
-              <div className="col">
-                <h2>Start building today</h2>
-                
-                <form>
-                  <div className="form-group input-submit">
-                    <label htmlFor="signupEmail">Email address</label>
-                    <input type="email" className="form-control" id="signupEmail" aria-describedby="emailHelp" placeholder="name@domain" />
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                  </div>
-                </form>
-                
-              </div> 
-            </div>
-          </section>
-
         </main>
       </section>
     </Layout>
