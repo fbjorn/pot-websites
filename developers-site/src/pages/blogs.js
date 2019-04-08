0b0f08c1f3b4627e3076efcdfe1ded07af6dc293
Mika Hamalainen <mika@equstom.fi>
2019-04-08 14:10:13 +0300

    Blog-listin styles

--- developers-site/src/pages/blogs.js
+++ developers-site/src/pages/blogs.js
@@ -3,34 +3,59 @@ import Link from "gatsby-link"
 import { graphql } from "gatsby"
 // import Helmet from "react-helmet"
 import SEO from '../components/seo'
-
 import Layout from '../components/layout'
 
-import '../styles/blog-listing.css'
+const styles = {
+  hex: {
+    transform: 'rotate(-5deg)'
+  },
+  poly: {
+    width: '100%',
+    fill: '#9176fe',
+    stroke: 'none',
+    strokeWidth: '0'
+  }
+}
 
 export default function Blogs({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
   return (
-    <Layout className="blog-posts">
-     <SEO title="Blogs" keywords={[`Platform of trust`, `developers`, 'blogs']} />
-      <section className="posts">
-        {posts
-          // .filter(post => post.node.frontmatter.title.length > 0)
-          .filter(post => post.node.frontmatter.type === "blog")
-          .map(({ node: post }) => {
-            return (
-              <div className="blog-post-preview" key={post.id}>
-                <h1>
-                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-                </h1>
-                <h2>{post.frontmatter.date}</h2>
-                <p>{post.excerpt}</p>
-                <Link to={post.frontmatter.path}>Read More...</Link>
-                <hr />
-              </div>
-            );
-          })}
+    <Layout className="container">
+      <SEO title="Blogs" keywords={[`Platform of trust`, `developers`, 'blogs']} />
+      <main className="page-content container dark-bg">
+        <section className="intro row">
+          <div className="side-content col-md-4">
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" version="1.1" style={styles.hex}>
+              <polygon style={styles.poly}  points="148,183.138438763306 52,183.138438763306 4,100 52,16.8615612366939 148,16.8615612366939 196,99.9999999999999 148,183.138438763306" />
+            </svg>
+          </div>
+          <div className="posts col-md-8">
+            <h1>Development Blogs</h1>
+          </div>
         </section>
+        <section className="blog-posts row">
+          <div className="side-content col-md-4">
+            
+          </div>
+          <div className="posts col-md-8">
+            {posts
+              // .filter(post => post.node.frontmatter.title.length > 0)
+              .filter(post => post.node.frontmatter.type === "blog")
+              .map(({ node: post }) => {
+                return (
+                  <div className="blog-post-preview" key={post.id}>
+                    <h3>
+                      <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                    </h3>
+                    <h6>{post.frontmatter.date}</h6>
+                    <p>{post.excerpt}</p>
+                    {/* <Link to={post.frontmatter.path}>Read More...</Link> */}
+                  </div>
+                );
+              })}
+          </div>
+        </section>
+      </main>
     </Layout>
   );
 }
