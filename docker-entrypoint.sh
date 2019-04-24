342057797c725f57c7e2737efadd2355e3561d02
Victor Vladescu <victorv@sbp-romania.com>
2019-04-24 10:23:36 +0300

    Include password in quotes to support special characters

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 if ! [ -z "${BASIC_USER_AUTH:-}" ] && ! [ -z "${BASIC_PASS_AUTH:-}" ];then
    sh -c "echo -n '${BASIC_USER_AUTH}:' >> /etc/nginx/sites/.htpasswd"
-   sh -c "openssl passwd -apr1 ${BASIC_PASS_AUTH} >> /etc/nginx/sites/.htpasswd"
+   sh -c "openssl passwd -apr1 \"${BASIC_PASS_AUTH}\" >> /etc/nginx/sites/.htpasswd"
 fi
 
 sed -i 's/$MARKETING_HOST/'"${MARKETING_HOST}"'/g' /etc/nginx/sites/*.conf
