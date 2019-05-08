98a1e39c56b83a674f3d5e8c49a9d7b383899355
Victor Vladescu <victorv@sbp-romania.com>
2019-05-08 09:36:14 +0300

    Change config for Netlify Github Auth

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,15 @@ if ! [ -z "${BASIC_USER_AUTH:-}" ] && ! [ -z "${BASIC_PASS_AUTH:-}" ];then
    sh -c "openssl passwd -apr1 \"${BASIC_PASS_AUTH}\" >> /etc/nginx/sites/.htpasswd"
 fi
 
-sed -i 's/$MARKETING_HOST/'"${MARKETING_HOST}"'/g' /etc/nginx/sites/*.conf
+sed -i 's+$MARKETING_HOST+'"${MARKETING_HOST}"'+g' /etc/nginx/sites/*.conf
+
+# Replace ENV variables in netlify config files
+sed -i 's+$WEBSITE_ROOT+'"${WEBSITE_ROOT}"'+g' /var/www/developers-site/cmsadmin/config.yml
+sed -i 's+$WEBSITE_REPO+'"${WEBSITE_REPO}"'+g' /var/www/developers-site/cmsadmin/config.yml
+sed -i 's+$AUTH_ENDPOINT+'"${AUTH_ENDPOINT}"'+g' /var/www/developers-site/cmsadmin/config.yml
+
+sed -i 's+$WEBSITE_ROOT+'"${WEBSITE_ROOT}"'+g' /var/www/marketing-site/cmsadmin/config.yml
+sed -i 's+$WEBSITE_REPO+'"${WEBSITE_REPO}"'+g' /var/www/marketing-site/cmsadmin/config.yml
+sed -i 's+$AUTH_ENDPOINT+'"${AUTH_ENDPOINT}"'+g' /var/www/marketing-site/cmsadmin/config.yml
 
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
