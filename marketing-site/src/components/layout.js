08689501996532726be889cc09510bb56dae6c61
Mika Hamalainen <mika@equstom.fi>
2019-05-14 17:13:03 +0300

    Emotion for StyledComponents

--- marketing-site/src/components/layout.js
+++ marketing-site/src/components/layout.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { Global, css } from "@emotion/core"
+import styled from "@emotion/styled"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -11,6 +13,11 @@ import Footer from './footer'
 import './layout.css'
 
 library.add(faHexagon)
+
+const Wrapper = styled("div")`
+margin: '0 auto',
+paddingTop: 0,
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -26,14 +33,14 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            paddingTop: 0,
-          }}
+        <Wrapper
+          // style={{
+          //   margin: `0 auto`,
+          //   paddingTop: 0,
+          // }}
         >
           {children}
-        </div>
+        </Wrapper>
         <Footer />
       </>
     )}
